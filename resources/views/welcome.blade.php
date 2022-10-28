07614bb7a26fed5aeb4745c0800032e0b16a13f0
Paulo <tamanini8@gmail.com>
2022-10-28 15:16:48 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,262 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+<body>
+
+<!--HEADER-->
+<header class="w-full fixed bg-blue-100 flex flex-1 items-center px-3 py-2">
+    <div class="flex flex-1 h-6"><img src="/img/logo.png.webp"></div>
+    <div class="flex md:space-x-6">
+        <div class="hidden md:flex">
+            <ul class="flex text-lg text-blue-800 font-semibold md:space-x-6 items-center">
+                <li><a href="#">Loans</a></li>
+                <li><a href="#">FAQ</a></li>
+            </ul>
+        </div>
+        <div><button class="apply-button">Apply Now</button></div>
+    </div>
+</header>
+
+<!--MAIN-->
+<main>
+    <!--BANNER-->
+    <div class="md:flex md:flex-1 pt-24 md:pt-10 text-center md:text-center md:justify-center">
+        <div class="w-1/3 md:pt-5 hidden md:flex place-items-center lg:content-end"><img src="/img/banner3.jpg"></div>
+        <div class="flex flex-col md:place-content-center md:space-y-8 md:pl-5 lg:pl-24">
+            <p class="text-grey-900">Fast funding.¹ Quick decisions.¹ Personal service.</p>
+            <p class="text-4xl md:text-6xl tracking-wide font-bold md:font-semibold text-blue-800 py-6 md:py-0">Online loans <br> designed for you</p>
+            <div class="flex flex-col mx-auto">
+                <button class="apply-button bg-yellow-500">Apply Now</button>
+                <p class="text-xs text-gray-500 pt-4 md:pt-2 md:place-items-center">Did you receive an offer code in the mail? Click here</p>
+            </div>
+
+        </div>
+    </div>
+
+    <!--UNEXPECTED EXPENSES-->
+    <div class="bg-gray-50 w-full px-4 md:px-24 lg:px-72 text-gray-800 md:text-lg">
+        <h1 class="text-blue-800 text-2xl md:text-4xl text-center font-bold py-8 md:py-12 ">Online Loans for Life’s Unexpected Expenses</h1>
+        <p class="pb-4 md:pb-12">Everyday consumers have access to financial support from OppLoans when they need it most. We offer small-dollar, online loans that are repaid in affordable installments.</p>
+        <ul class="space-y-4 md:space-y-12">
+            <li>
+                <p><b>A simple and straightforward process:</b> When it comes to your finances, we don't believe in surprises.</p>
+            </li>
+            <li>
+                <p><b>Establish credit history through repayments:</b> We’re interested in making an impact on your financial future. That’s why we report loan repayments to all three major credit bureaus.</p>
+            </li>
+            <li>
+                <p><b>Loan support at every step:</b> With top-rated customer support, we’re by your side at every part of the process. Our Loan Advocates are here for you. We succeed when you do.</p>
+            </li>
+            <li>
+                <p><b>Develop your financial skills:</b> Learn how to make better financial decisions with essential educational resources. You’ll feel empowered to take control of your financial situation.</p>
+            </li>
+        </ul>
+        <h2 class="pt-8 md:pt-12 pb-4 text-center text-2xl text-blue-800 font-semibold">We’re in this together</h2>
+        <p class="pb-8 md:pb-12">Our goal is to help our customers champion their finances. Partner with us on your financial journey. This isn’t just a loan. This is an opportunity for a better financial future.</p>
+    </div>
+
+    <!--APPLY FOR-->
+    <div class="bg-purple-100 w-full px-4">
+        <h1  class="text-blue-800 text-2xl md:text-4xl text-center font-bold px-10 md:pt-8 md:pb-4 py-8">Apply for an Online Loan Through OppLoans</h1>
+        <p class="text-center">We help people find a solution for their individual financial needs.</p>
+        <div>
+            <div class="py-8">
+                <img src="/img/familia.webp">
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <div class="family-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                            <h3 class="apply-headers">Easy to Apply</h3>
+                        </div>
+                        <p>A simplified application process. Apply online from start to finish.</p>
+                    </li>
+                    <li>
+                        <div class="family-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
+                            </svg>
+                            <h3 class="apply-headers">Fast Funding</h3>
+                        </div>
+
+                        <p>If approved, receive money in your account as soon as the next business day!1</p>
+                    </li>
+                    <li>
+                        <div class="family-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z" />
+                            </svg>
+                            <h3 class="apply-headers">Loans that fit your financial needs</h3>
+                        </div>
+                        <p>We offer loans from $500 to $4,000.4</p>
+                    </li>
+                    <li>
+                        <div class="family-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                            </svg>
+                            <h3 class="apply-headers">No Hidden Fees</h3>
+                        </div>
+                        <p>We’re committed to transparency. No origination fees. No prepayment penalties. You’ll know what to expect.</p>
+                    </li>
+                    <li>
+                        <button class="apply-button">Apply Now</button>
+                        <p class="text-xs text-gray-500">Applying does NOT affect your FICO® credit score!2</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <!--FAQ-->
+    <div class="bg-pink-100 w-full">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+        </svg>
+        <ul>
+            <li>
+                <h3>What is an online personal loan?</h3>
+                <p>Online personal loans are unsecured installment loans offered by online lenders and some traditional banks. Broadly, online credit products may take the form of a line of credit, a cash advance, or other loan options.
+
+                    Online personal loan offers should clearly disclose the loan's APR (annual percentage rate), the term, and any fees such as finance charges.</p>
+            </li>
+            <li>
+                <h3>How are OppLoans online personal loans different?</h3>
+                <p>We truly care about our customers and work hard to be flexible for each of our customer's unique situations. We want to make sure borrowers get the best loan for them, whether it's with OppLoans or a different online lender.
+
+                    Unlike some financial institutions (and quite unlike payday loans5), OppLoans charges no origination fees or prepayment penalties. Our loans charge simple, fixed rate interest and contain no hidden fees.
+
+                    OppLoans doesn't base funding decisions exclusively on FICO credit scores or credit history.
+
+                    We also report payments to all three major credit bureaus to help borrowers build their credit history.6
+
+                    And if you opt into our "Turn Up Program", we'll proactively see if your loan application would qualify you for a loan at a cheaper interest rate with one of our lending partners.</p>
+            </li>
+            <li>
+                <h3>What are the eligibility requirements to apply for an online personal loan?</h3>
+                <p>The first step in applying for a loan through OppLoans is to fill out our online loan application. To be eligible for a lending product through OppLoans, potential borrowers must:
+
+                    Be 18 years or older
+                    Have a bank account (checking account or saving account)
+                    Reside in one of the states in which we currently operate
+                    Have a regular source of income (Income can be from employment or from benefits including Social Security, Disability, etc. Please note: alimony, child support, or separate maintenance income is optional to include)
+                    Receive income through direct deposit</p>
+            </li>
+            <li>
+                <h3>What can you use an online personal loan for?</h3>
+                <p>Online personal loans can be used for a wide variety of purposes. Maybe you're looking for a loan to help consolidate debt from multiple credit cards, or maybe you need to fund an expensive home improvement or car repair. Also, if you're looking to turn bad credit into good credit, the right online personal loan may help you do that.
+
+                    OppLoans customers have used their loan amounts for:
+
+                    Car repair loans
+                    Home repair loans
+                    Debt consolidation loans
+                    Loans to help pay off medical debt
+                    Whatever your reason for searching online lenders (and whether you have excellent credit, bad credit, or you land somewhere in the middle), there are terms and conditions of the loan that you'll want to understand. Borrowers should always be clear on the interest rate, repayment terms, and whether or not the lender will run a hard or soft credit check.</p>
+            </li>
+            <li>
+                <h3>How much can I apply for?</h3>
+                <p>Consumer products offered through the OppLoans website range from $500 to $4,000, but your eligibility will depend on your state of residence, your income and ability to repay, and your creditworthiness. For more information about the product options and lender(s) in your state, please visit our
+                    <a href="#">Rates and Terms page</a>.</p>
+            </li>
+            <li>
+                <h3>How does repayment of a personal loan work?</h3>
+                <p>OppLoans believes in matching borrowers with the best possible loan for them. If we're able to provide you with an online personal loan product, we want to make sure your repayment options are both clear and affordable across the life of the loan term.
+
+                    OppLoans offers multiple recurring payment options including ACH and paper check options. For single payments, debit cards are also accepted.</p>
+            </li>
+        </ul>
+    </div>
+
+    <!--WO WE SERVE-->
+    <div class="bg-red-300 w-full">
+        <h1>Who We Serve</h1>
+        <p>
+            Credit remains out of reach for everyday consumers. Learn how OppLoans provides the tools so people can take control of their finances.
+        </p>
+        <img src="">
+        <div>
+            <div>
+                <p>logo</p>
+                <p>60 million U.S. adults
+                    lack access to traditional credit*</p>
+            </div>
+            <div>
+                <p>logo</p>
+                <p>64% of U.S. consumers
+                    live paycheck to paycheck*</p>
+            </div>
+            <div>
+                <p>logo</p>
+                <p>44% of U.S. adults have savings
+                    to cover a $1,000 unplanned expense*</p>
+            </div>
+        </div>
+    </div>
+</main>
+
+<!--FOOTER-->
+<footer class="bg-blue-300 w-full">
+    <div>
+        <span>logo</span>
+        <span>social media</span>
+    </div>
+    <div>
+        <h3>OppLoans Offers</h3>
+        <a href="#">Online Personal Loans</a>
+        <a href="#">Rates and Terms</a>
+    </div>
+    <div>
+        <h3>Learn More About</h3>
+        <a href="#">Bad Credit Loans</a>
+        <a href="#">No Credit Check Loans</a>
+        <a href="#">Installment Loans</a>
+    </div>
+    <div>
+        <h3>Legal</h3>
+        <a href="#">Investor Relations</a>
+        <a href="#">Accessibility</a>
+        <a href="#">Security</a>
+        <a href="#">Contact</a>
+    </div>
+
+    <!--SMALL TERMS-->
+    <div class="bg-pink-400 w-full">
+        <p>
+            © 2022 Opportunity Financial, LLC. All Rights Reserved. Use of Opportunity Financial, LLC is subject to our
+            <a href=""#>Terms of Use</a> and <a href=""#>Privacy Policy</a>.
+
+            <a href=""#>California Disclosures and Privacy Policy</a> | California Consumers can opt-out of the sale of personal information by clicking <a href=""#>Do Not Sell My Personal Information</a>.
+
+            Applications submitted on this website may be originated by one of several lenders, including: Opportunity Financial LLC, a licensed lender in certain states; or one of our <a href=""#>lending partners</a>. All loans originated by our lending partners will be serviced by OppLoans.
+
+            NV Residents: The use of high-interest loans services should be used for short-term financial needs only and not as a long-term financial solution. Customers with credit difficulties should seek credit counseling before entering into any loan transaction.
+
+            1Subject to credit approval and verification. Actual approved loan amount and terms are dependent on standard underwriting guidelines and credit policies. Funds are typically deposited via ACH for delivery for next business day if verification is completed and final approval occurs before 8:00 PM CT, Monday-Friday. Availability of the funds is dependent on how quickly your bank processes the transaction.
+
+            2OppLoans may use credit report information provided by Clarity Services and Experian as part of the application process to determine your creditworthiness. Neither credit inquiry will appear as a hard credit inquiry on your Experian credit report and therefore they will not affect your FICO score.
+
+            4Installment loan amounts typically range from $500 to $4,000. APR ranges from 59% to 160%. Installment loan lengths range from 9 to 18 months. Example: A $2,000 installment loan repayable in 9 monthly payments would have an APR of 160% and monthly payments of $394.58. <a href=""#>Rates and terms vary by state</a>. This is an expensive form of credit and you should determine whether our product(s) meets your financial needs.
+
+            5According to the Consumer Federation America, a non-profit consumer advocacy group, payday loans range in size from $100 to $1,000, depending on state legal maximums, and carry an average annual percentage rate (APR) of 400%. The maximum APR for a loan offered through and serviced by OppLoans is 160% and loan sizes range from $500 to $4,000. <a href=""#>https://paydayloaninfo.org/how-payday-loans-work/</a>.
+
+            6OppLoans reports customer payment history to the three major credit bureaus. On-time payments may improve credit score.
+
+            USA PATRIOT ACT NOTICE: IMPORTANT INFORMATION ABOUT PROCEDURES FOR OPENING A NEW ACCOUNT
+
+            To help the government fight the funding of terrorism and money laundering activities, Federal law requires all financial institutions to obtain, verify, and record information that identifies each person who opens an account. What this means for you: When you open an account, we will ask for your name, address, date of birth, and other information that will allow us to identify you. We may also ask to see your driver's license or other identifying documents.
+
+            If you have questions or concerns, please contact the Opportunity Financial Customer Support Team by phone at (800) 990-9130, Monday – Friday, 7 a.m. – 11:30 p.m. and Saturday and Sunday between 9 a.m. – 5:00 p.m. Central Time, or by sending an email to <a href=""#>info@opploans.com</a>.
+        </p>
+    </div>
+</footer>
+
+</body>
+</html>
